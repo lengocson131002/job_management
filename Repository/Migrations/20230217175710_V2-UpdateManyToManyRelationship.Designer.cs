6858b550ab165d43d8379fd516fde960815fa655
lengocson <leson131002@gmail.com>
2023-02-18 01:00:22 +0700

    Add many to many relation ship

--- Repository/Migrations/20230217175710_V2-UpdateManyToManyRelationship.Designer.cs
+++ Repository/Migrations/20230217175710_V2-UpdateManyToManyRelationship.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
@@ -11,9 +12,10 @@ using Repository;
 namespace Repository.Migrations
 {
     [DbContext(typeof(JobManagementDBContext))]
-    partial class JobManagementDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230217175710_V2-UpdateManyToManyRelationship")]
+    partial class V2UpdateManyToManyRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Repository/Migrations/20230217175710_V2-UpdateManyToManyRelationship.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class V2UpdateManyToManyRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resume_JobDescription_JobDescriptionId",
+                table: "Resume");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skill_JobDescription_JobDescriptionId",
+                table: "Skill");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skill_Resume_ResumeId",
+                table: "Skill");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skill_JobDescriptionId",
+                table: "Skill");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skill_ResumeId",
+                table: "Skill");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Resume_JobDescriptionId",
+                table: "Resume");
+
+            migrationBuilder.DropColumn(
+                name: "JobDescriptionId",
+                table: "Skill");
+
+            migrationBuilder.DropColumn(
+                name: "ResumeId",
+                table: "Skill");
+
+            migrationBuilder.DropColumn(
+                name: "JobDescriptionId",
+                table: "Resume");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Application",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Application",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Application");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Application");
+
+            migrationBuilder.AddColumn<long>(
+                name: "JobDescriptionId",
+                table: "Skill",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "ResumeId",
+                table: "Skill",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "JobDescriptionId",
+                table: "Resume",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skill_JobDescriptionId",
+                table: "Skill",
+                column: "JobDescriptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skill_ResumeId",
+                table: "Skill",
+                column: "ResumeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resume_JobDescriptionId",
+                table: "Resume",
+                column: "JobDescriptionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resume_JobDescription_JobDescriptionId",
+                table: "Resume",
+                column: "JobDescriptionId",
+                principalTable: "JobDescription",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skill_JobDescription_JobDescriptionId",
+                table: "Skill",
+                column: "JobDescriptionId",
+                principalTable: "JobDescription",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skill_Resume_ResumeId",
+                table: "Skill",
+                column: "ResumeId",
+                principalTable: "Resume",
+                principalColumn: "Id");
+        }
+    }
+}
