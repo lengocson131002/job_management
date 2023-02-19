17e345ea87add91d4330829a9f938224b0fad2cc
Nguyễn Trung Hiếu <nthieu1332002@gmail.com>
2023-02-19 11:12:32 +0000

    List, update company - (not validation, pagination yet)

--- JobApplicationManagement/Controllers/CompanyController.cs
+++ JobApplicationManagement/Controllers/CompanyController.cs
@@ -21,5 +21,40 @@ namespace JobApplicationManagement.Controllers
             List<Company> companies = _companyRepository.GetAll().ToList();
             return View(companies);
         }
+        public IActionResult CompanyDetail(long id)
+        {
+            Company company = _companyRepository.GetById(id);
+            return View(company);
+        }
+        public IActionResult AddCompany()
+        {
+            return View();
+        }
+
+        public IActionResult CreateCompany(Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("AddCompany", company);
+            }
+            company.Longitude = 0;
+            company.Latitude = 0;
+            company.CreatedAt = DateTime.Now;
+            _companyRepository.Insert(company);
+            _companyRepository.Save();
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult UpdateCompany(Company company)
+        {
+            company.Longitude = 0;
+            company.Latitude = 0;
+            company.ModifiedAt = DateTime.Now;
+            _companyRepository.Update(company);
+            _companyRepository.Save();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
