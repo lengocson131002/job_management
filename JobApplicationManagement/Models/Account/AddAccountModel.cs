06c9b258caac2a4a2aabd3c85e5ada746495304a
Nguyễn Trung Tín <se.nttin1811@gmail.com>
2023-03-04 12:09:58 +0000

    add filter redirect url, fix ui

--- JobApplicationManagement/Models/Account/AddAccountModel.cs
+++ JobApplicationManagement/Models/Account/AddAccountModel.cs
@@ -10,6 +10,8 @@ namespace JobApplicationManagement.Models.Auth
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
         [EmailAddress]
         [Required]
         public string Email { get; set; }
